62aca90172a141503a4aaabc4025fcd141358600
李铭昕 <715557344@qq.com>
2024-01-06 10:00:16 +0800

    2024-01-06 打包

---
+++ precache-manifest.38d646ca15317359331ab220c6972ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65894cca4c366d4e1e7f268c9b25e8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d63b169f851615817b6e",
+    "url": "/static/css/2.3d9623d2.chunk.css"
+  },
+  {
+    "revision": "8a4deb4add2e94d78c11",
+    "url": "/static/css/main.78e57de2.chunk.css"
+  },
+  {
+    "revision": "d63b169f851615817b6e",
+    "url": "/static/js/2.224fed20.chunk.js"
+  },
+  {
+    "revision": "8a4deb4add2e94d78c11",
+    "url": "/static/js/main.714ae56a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ecbb1b5a97f3f27948b70df3e34e997.js"
+  "/precache-manifest.38d646ca15317359331ab220c6972ba3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
